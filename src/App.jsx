f3b85ac39df346e43e7e9cdcb2d10cdabd94aa45
MayVarela16 <loly_varela@hotmail.com>
2024-03-10 14:49:39 +0100

    final

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,61 @@
 import './App.css'
 
+import{useState}from 'react';
+import ReactDOM from 'react-dom/client'
+
+
+
 function App() {
- //Aquí tu código
+ const [name, setName] = useState('Sofía');
+ const [newName, setNewName] = useState('');
+
+ const handleNameChange = () => {
+    if (newName.trim() !== '') {
+        setName(newName);
+        setNewName('');
+      }
+    };
+
+    const handleListClick = (newName) => {
+        setNewName(newName); 
+      };
+
+
+      const changeName = (e) => {
+        e.preventDefault();
+        handleNameChange();
+      };
+
+
+  return (
+<div className= 'Main-container'>
+    <div className= 'container'>
+      <h2 className="title">Teacher's Name: {name}</h2>
+
+      <form className="formulario" onSubmit={changeName}>
+        <input
+          type="text"
+          value={newName}
+          onChange={(e) => setNewName(e.target.value)}
+          placeholder="add a name"
+          className='input'
+        />
+      </form>
+
+      <button onClick={handleNameChange} className='boton'>
+        Cambiar Nombre
+      </button>
+
+      <ul className='lista'>
+        <li onClick={() => handleListClick('Data')}>Data</li>
+        <li onClick={() => handleListClick('Reyes')}>Reyes</li>
+        <li onClick={() => handleListClick('Yolanda')}>Yolanda</li>
+      </ul>
+    </div>
+</div>
+  );
 }
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
 
 export default App
